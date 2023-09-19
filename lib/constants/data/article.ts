a6e11fb0372f949d8e27902ac19b34a0b2f93fb0
Heo Jin Hyeok <gjwlsgur123@gmail.com>
2023-09-19 16:12:27 +0900

    Dummy data 정리

---
+++ lib/constants/data/article.ts
@@ -0,0 +1,10 @@
+export const visionInfo = {
+  title: "KS Interior vision",
+  bgInfo: { src: "/img/vision/vision_bg.svg", alt: "Vision" },
+  contents:
+    "Lorem ipsum dolor sit amet consectetur. Eu cursus arcu maecenas tristique eu sed purus. Tempus neque in ac id scelerisque eget justo tempor nisl. Velit sapien facilisi magnis bibendum. Tincidunt facilisi nisl pharetra quam morbi.",
+}
+
+export const bestInfo = {
+  title: "Best Interior Trend",
+}
---
+++ lib/constants/data/card.ts
@@ -0,0 +1,12 @@
+export const firstCardInfo = {
+  title: "트렌디 화이트",
+  imgInfo: { src: "/img/card/first_card.svg", alt: "first_card" },
+  content:
+    "Lorem ipsum dolor sit amet consectetur. Neque accumsan lectus tellus a dictumst arcu. Venenatis nec volutpat euismod a.",
+}
+export const secondCardInfo = {
+  title: "트렌디 내추럴",
+  imgInfo: { src: "/img/card/second_card.svg", alt: "second_card" },
+  content:
+    "Lorem ipsum dolor sit amet consectetur. Neque accumsan lectus tellus a dictumst arcu. Venenatis nec volutpat euismod a.",
+}
